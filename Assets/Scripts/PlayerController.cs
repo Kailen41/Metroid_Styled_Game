47dcc12c283e60b6cf58edffbe271582daa0b370
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-07-29 20:54:59 -0700

    Player Jumping with checks
    -Player can now jump using the space bar.
    -Using the rigidbody, jump force variable is applied to the Y axis when the player presses the space bar.
    -Ground check, layer mask, and bool used to determine if player is in the air or on the ground. Sphere used to calculate if the player is on the ground by hitting objects with the ground layer attached to them.
    -When the sphere collides with the tile set, the player can jump. If the sphere does not collide with it, the player can not jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
-
     private Rigidbody2D theRB;
 
     public float moveSpeed;
+    [Header("Jump Info")]
     public float jumpForce;
+    public Transform groundPoint;
+    public LayerMask groundCheck;
+
+    private bool isOnGround;
 
     private void Awake()
     {
@@ -24,5 +28,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, groundCheck);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
     }
 }
