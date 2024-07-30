6e9f1240d4d3ca6d17f85023903dc89abc4e9869
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-07-29 21:31:03 -0700

    Idle, Run, Jump (WIP), animations with sprite flip
    -Idle, Run, and partial jump animation created for project. Transitions with parameters created in the animator.
    -Movement is based on speed that is greater than 0.1, but using Mathf.Abs, it allows for movement on X axis going both ways and not just positive.
    -Partial jump animation based on bool that checks if the player is on the ground, and sets the animator bool to true or false based on that check.
    -Sprite is flipped based on local scale. If the player has a speed on the X greater than 0, the player's sprite will face the right. If it is less than 0, it will face the left. Sprite stays facing either direction after initial turn and the buttons are released.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
+
+    private Animator theAnim;
     private Rigidbody2D theRB;
 
     public float moveSpeed;
@@ -18,22 +20,39 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         theRB = GetComponent<Rigidbody2D>();
-    }
-
-    void Start()
-    {
-        
+        theAnim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        PlayerMovementAndAnimationController();
+    }
+
+    private void PlayerMovementAndAnimationController()
+    {
+        // Move Sideways
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
 
+        // Handle direction change
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        // Checking if on the ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, groundCheck);
 
+        // Jumping
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
         }
+        
+        theAnim.SetBool("isOnGround", isOnGround);
+        theAnim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
     }
 }
